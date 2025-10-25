bf6bbaf555caf92df1251f360902a3e1755c4c28
jashinbhatia <jashinbhatia4567@gmail.com>
2025-10-25 22:04:06 +0530

    koanf config setup

---
+++ apps/backend/internal/config/config.go
@@ -0,0 +1,47 @@
+package config
+
+type Config struct {
+	Primary     Primary           `koanf:"primary" validate:"required"`
+	Server      ServerConfig      `koanf:"server" validate:"required"`
+	Database    DatabaseConfig    `koanf:"database" validate:"required"`
+	Auth        AuthConfig        `koanf:"auth" validate:"required"`
+	Redis       RedisConfig       `koanf:"redis" validate:"required"`
+	Integration IntegrationConfig `koanf:"integration" validate:"required"`
+	//Observability *ObservabilityConfig `koanf:"observability"`
+}
+
+type Primary struct {
+	Env string `koanf:"env" validate:"required"`
+}
+
+type ServerConfig struct {
+	Port               string   `koanf:"port" validate:"required"`
+	ReadTimeout        int      `koanf:"read_timeout" validate:"required"`
+	WriteTimeout       int      `koanf:"write_timeout" validate:"required"`
+	IdleTimeout        int      `koanf:"idle_timeout" validate:"required"`
+	CORSAllowedOrigins []string `koanf:"cors_allowed_origins" validate:"required"`
+}
+
+type DatabaseConfig struct {
+	Host            string `koanf:"host" validate:"required"`
+	Port            int    `koanf:"port" validate:"required"`
+	User            string `koanf:"user" validate:"required"`
+	Password        string `koanf:"password"`
+	Name            string `koanf:"name" validate:"required"`
+	SSLMode         string `koanf:"ssl_mode" validate:"required"`
+	MaxOpenConns    int    `koanf:"max_open_conns" validate:"required"`
+	MaxIdleConns    int    `koanf:"max_idle_conns" validate:"required"`
+	ConnMaxLifetime int    `koanf:"conn_max_lifetime" validate:"required"`
+	ConnMaxIdleTime int    `koanf:"conn_max_idle_time" validate:"required"`
+}
+type RedisConfig struct {
+	Address string `koanf:"address" validate:"required"`
+}
+
+type IntegrationConfig struct {
+	ResendAPIKey string `koanf:"resend_api_key" validate:"required"`
+}
+
+type AuthConfig struct {
+	SecretKey string `koanf:"secret_key" validate:"required"`
+}
